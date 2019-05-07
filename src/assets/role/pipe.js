dabcb8c13dfd911f6b1c790d4b557326ab50d613
life0727 <mmlife0727@gmail.com>
2019-05-07 19:21:17 +0800

    userdata

--- src/assets/role/pipe.js
+++ src/assets/role/pipe.js
@@ -4,11 +4,8 @@ import userData from '../userData/data'
 class pipe extends canvas{
     constructor(){
         super()
-        this.speed = 1;
-        this.width_down = super.resources.pipe_down.width;//上管子的宽
-        this.width_up = super.resources.pipe_up.width;//下管子的宽
+        this.speed = 2;
         this.height_down = super.resources.pipe_down.height;//上管子的高
-        this.height_up = super.resources.pipe_up.height;//下管子的高
         this.x = this.canvas.width;
         this.y_down = - Math.random() * (this.height_down - 110); //上馆子最低高度不低于110
         this.y_up = this.canvas.height * userData.ScreenRadio + this.y_down - userData.Gap;
@@ -20,7 +17,7 @@ class pipe extends canvas{
         console.log('pie')
     }
     run(){
-        this.x -= 2;
+        this.x -= this.speed;
     }
     render(){
         this.ctx.drawImage(this.resources.pipe_down,this.x,this.y_down);
